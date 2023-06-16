d9cad22abecafbf2ae961f98196664b9e7298e3d
SolimanHamed <soliman.zuko@gmail.com>
2023-06-16 10:44:35 -0700

    Numirc

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,18 +2,18 @@
 
 /**
  * main - entry point
- * Description: print 0 - 9 
+ * Description: print 0 - 9
  * Return: 0 (success)
  */
 
 int main(void)
 {
-       int num = 0;
-	do
-	{
-		printf("%d",num);
+	int num = 0;
+
+	do {
+		printf("%d", num);
 		num++;
-	}while(num < 10);
+	} while (num < 10);
 	printf("\n");
 	return (0);
 }
