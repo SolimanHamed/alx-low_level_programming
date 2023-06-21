1ddd7810bd287e856f07028ccfdef4bc6c867a1b
SolimanHamed <soliman.zuko@gmail.com>
2023-06-21 16:40:47 -0700

    Nest loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,39 +1,38 @@
 #include "main.h"
 
 /*
- * times_table function - print the 9 times table
+ * times_table - Prints the 9 times table
  *
- * Example Table
- * 0, 0, 0, 0, ..
- * 0, 1, 2, 3, ..
- *
-*/
-
+ * Description:
+ * This function prints the multiplication table for the numbers 0 to 9.
+ * Each row represents a number,
+ * and each column represents the product of that number
+ * multiplied by the column index.
+ */
 void times_table(void)
 {
-	int num, mult, prod;
+int num, mult, prod;
 
-	for (num = 0; num <= 9; num++)
-	{
-		_putchar(48);
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar('.');
-			_putchar(' ');
+for (num = 0; num <= 9; num++)
+{
+_putchar(48); /* Print the initial '0' character */
+for (mult = 1; mult <= 9; mult++)
+{
+_putchar(','); /* Print the comma separator */
+_putchar(' '); /* Print the space separator */
 
-			prod = num * mult;
+prod = num * mult;
 
-			/*
-			 * put space if product is a single number
-			 * place the first digit if it's two numbers
-			 */
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + 48); /*get the first digit*/
+/*
+ * If the product is a single digit, print an additional space
+ * before the number to align the columns.
+ */
+if (prod <= 9)
+_putchar(' ');
 
-			_putchar((prod % 10) + 48); /*get the second digit*/
-		}
-		_putchar('\n');
-	}
+_putchar((prod / 10) + 48); /* Print the first digit of the product */
+_putchar((prod % 10) + 48); /* Print the second digit of the product */
+}
+_putchar('\n'); /* Move to the next line after each row */
+}
 }
