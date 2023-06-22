cffedfb40904456f71b4ba1006b89ef596c8cfb5
SolimanHamed <soliman.zuko@gmail.com>
2023-06-22 16:11:06 -0700

    more function more Nest loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diganoal - print a diagonal line
+ *
+ * @n: is the number of times the / character
+ * should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int postn, space;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= postn; space++)
+				putchar(' ');
+			putchar(92); /*is equal to '/' char*/
+			putchar('\n');
+		}
+	}
+}
