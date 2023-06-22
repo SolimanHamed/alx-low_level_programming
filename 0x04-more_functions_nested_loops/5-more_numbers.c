e007f5d18790eedc6f4fe08bb9919c900ad07ff1
SolimanHamed <soliman.zuko@gmail.com>
2023-06-22 16:00:08 -0700

    more function more Nest loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,11 +18,11 @@ void more_numbers(void)
 			num = count;
 			if (count > 9)
 			{
-				_putchar(1 + 48);
+				putchar(1 + 48);
 				num = count % 10;
 			}
-			_putchar(num + 48);
+			putchar(num + 48);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
