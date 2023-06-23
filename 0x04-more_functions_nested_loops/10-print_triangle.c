d4b994bafa91d5bf74380718dde14e49bb445bbe
SolimanHamed <soliman.zuko@gmail.com>
2023-06-23 13:02:09 -0700

    more function more Nest loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,7 +13,7 @@ void print_triangle(int size)
 	int hight, base;
 
 	if (size <= 0)
-		_putchar('\n');
+		putchar('\n');
 	else
 	{
 		for (hight = 1; hight <= size; hight++)
@@ -21,9 +21,9 @@ void print_triangle(int size)
 			for (base = 1; base <= size; base++)
 			{
 				if ((hight + base) <= size)
-					_putchar(' ');
+					putchar(' ');
 				else
-					_putchar('#');
+					putchar('#');
 			}
 			putchar('\n');
 		}
