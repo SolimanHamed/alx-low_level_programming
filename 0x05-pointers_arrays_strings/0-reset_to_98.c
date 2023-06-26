125fb45c918097782576d5c4d4ac28f48892c114
SolimanHamed <soliman.zuko@gmail.com>
2023-06-26 16:26:46 -0700

    Pointers and Arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value to 98
+ * Description: a function that takes address and modifiy the value
+ * @n: a pointer that takes address and update to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
