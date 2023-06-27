54d44ab830812076482fdb9e1e99287f568a8770
SolimanHamed <soliman.zuko@gmail.com>
2023-06-27 16:29:19 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,7 +10,7 @@ int main(void)
 	int i = 0;
 	srand(time(NULL));
 
-	char password[PASSWORD_LENGTH +1];
+	char password[PASSWORD_LENGTH + 1];
 
 	for (i = 0; i < PASSWORD_LENGTH; i++)
 		password[i] = '!' + rand() % 94;
