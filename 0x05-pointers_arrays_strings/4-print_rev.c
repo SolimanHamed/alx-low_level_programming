a670e6b3229e5a736140f2ced13160513c7c9673
SolimanHamed <soliman.zuko@gmail.com>
2023-06-26 17:39:39 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,10 +15,9 @@ void print_rev(char *s)
 		{
 			length++;
 		}
-		while (length > 0)
+		for (int i = length - 1; i >= 0; i--)
 		{
-			putchar(s[length]);
-			length--;
+			putchar(s[i]);
 		}
 	}
 	putchar('\n');
