f87683b224b4e856ce4f5284a96cc942393316a6
SolimanHamed <soliman.zuko@gmail.com>
2023-06-27 16:45:14 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,19 +2,46 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-#define PASSWORD_LENGTH 90
-
 int main(void)
 {
-	int i = 0;
-	srand(time(NULL));
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	char password[PASSWORD_LENGTH + 1];
+	srand(time(0));
 
-	for (i = 0; i < PASSWORD_LENGTH; i++)
-		password[i] = '!' + rand() % 94;
-	password[PASSWORD_LENGTH] = '\0'; // Null terminator
-	
-	printf("Tada! Congrats\n");
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if(password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+
+			for (index = 0; password[index]; index++)
+			{
+				if (password[index] >= (33 + diff_half2)) 
+				{
+					password[index] -= diff_half2;
+					break;
+				}
+			}
+	}
+	printf("%s",password);
 }
