19f54032ca56961e20dc3007db8cdc8d3ec4c994
SolimanHamed <soliman.zuko@gmail.com>
2023-06-26 17:09:15 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * _strLen - return the length of string
+ * _strlen - return the length of string
  * Description: return length of a string
  * @s: check if s is null or not
  * Return: length of string
  */
 
-int _strLen(char *s)
+int _strlen(char *s)
 {
 	int length = 0;
 
