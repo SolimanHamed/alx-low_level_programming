b3c33aab6c97934061760b814c93da22e1414678
SolimanHamed <soliman.zuko@gmail.com>
2023-06-27 15:08:31 -0700

    Pointers and Arrays

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+ * _atoi - a function that convert string to intger
+ * @s: an inuput from user to check and convert
+ * Return: (Result);
+ */
+
+int _atoi(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	while (*s != '\0' && (*s < '0' || *s > '9'))
+		s++;
+	int sign = 1;
+
+	if (*(s - 1) == '-')
+		sign = -1;
+
+	int result = 0;
+
+	while (*s != '\0' && *s >= '0' && *s <= '9')
+	{
+		int digit = *s - '0';
+
+		if ((result > 0 && result > (INT_MAX - digit) / 10)
+				|| (result < 0 && result < (INT_MIN + digit) / 10))
+		{
+			printf("Overflow occurred\n");
+			return (0);
+		}
+		result = result * 10 + (sign * digit);
+		s++;
+	}
+	return (result);
+}
