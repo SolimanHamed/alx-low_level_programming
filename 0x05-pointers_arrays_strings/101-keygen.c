22429e3aab1114103bd29d44c62d6dd855334829
SolimanHamed <soliman.zuko@gmail.com>
2023-06-27 17:12:41 -0700

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define PASSWORD_LENGTH 10
 
-int main(void)
-{
+char* generate_password(int length) {
+    char* password = (char*)malloc((length + 1) * sizeof(char));
+    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=";
+    int charset_length = strlen(charset);
+    
     srand(time(NULL));
-
-    char password[PASSWORD_LENGTH + 1]; // +1 for null terminator
-
-    for (int i = 0; i < PASSWORD_LENGTH; i++)
-    {
-        // Generate a random character between '!' and '~'
-        password[i] = '!' + rand() % 94;
+    for (int i = 0; i < length; i++) {
+        int random_index = rand() % charset_length;
+        password[i] = charset[random_index];
     }
+    password[length] = '\0';
+    
+    return password;
+}
 
-    password[PASSWORD_LENGTH] = '\0'; // Null terminator
+int is_valid_password(const char* password) {
+    // Insert your validation logic for 101-crackme password here
+    // Return 1 if the password is valid, otherwise return 0
+}
 
-    printf("%s\n", password);
+int main() {
+    int password_length = PASSWORD_LENGTH;
+    char* password = generate_password(password_length);
+    
+    while (!is_valid_password(password)) {
+        free(password);
+        password = generate_password(password_length);
+    }
+    
     printf("Tada! Congrats\n");
-
+    printf("Generated Password: %s\n", password);
+    
+    free(password);
+    
     return 0;
 }
 
