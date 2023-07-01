d8f42750a148d1141c1efb5d0fffda446e3e9c4c
SolimanHamed <soliman.zuko@gmail.com>
2023-06-30 19:37:53 -0700

    0x06 Pointers and Arrays and Function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_number - print numbers chars
  * @n: integer params
@@ -22,5 +22,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	putchar((n1 % 10) + '0');
 }
