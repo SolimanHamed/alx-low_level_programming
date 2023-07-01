c1df0bba0eef444831f204f773ad31bfba2c4323
SolimanHamed <soliman.zuko@gmail.com>
2023-06-30 19:41:00 -0700

    0x06 Pointers and Arrays and Function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,17 +10,11 @@ void print_number(int n)
 {
 	unsigned int n1;
 
-	n1 = n;
+	n1 = (n < 0) ? -n : n; // Handle negative numbers
 
 	if (n < 0)
-	{
 		putchar('-');
-		n1 = -n;
-	}
-
 	if (n1 / 10 != 0)
-	{
 		print_number(n1 / 10);
-	}
 	putchar((n1 % 10) + '0');
 }
